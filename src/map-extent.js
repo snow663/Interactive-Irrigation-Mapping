855ca6b75094a8c0ab9f5b5e789e730b5de63e96
snow663 <49832788+snow663@users.noreply.github.com>
2026-06-27 23:55:02 -0600

    Add alternate topo and GIS basemaps

--- src/map-extent.js
+++ src/map-extent.js
@@ -1,8 +1,33 @@
 (() => {
   if (!window.L || !L.Map) return;
 
-  const baseSetMaxBounds = L.Map.prototype.setMaxBounds;
+  const usgsTopo = () => L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: 16,
+    attribution: 'USGS The National Map'
+  });
 
+  const usgsImageryTopo = () => L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: 16,
+    attribution: 'USGS The National Map'
+  });
+
+  const esriTopo = () => L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: 19,
+    attribution: 'Tiles &copy; Esri'
+  });
+
+  const baseLayersControl = L.control.layers;
+  L.control.layers = function patchedLayerControl(baseLayers = {}, overlays = {}, options = {}) {
+    const expandedBaseLayers = {
+      ...baseLayers,
+      'USGS Topo': usgsTopo(),
+      'USGS Imagery Topo': usgsImageryTopo(),
+      'Esri Topographic': esriTopo()
+    };
+    return baseLayersControl.call(this, expandedBaseLayers, overlays, options);
+  };
+
+  const baseSetMaxBounds = L.Map.prototype.setMaxBounds;
   L.Map.prototype.setMaxBounds = function setExtentMaxBounds(bounds) {
     const result = baseSetMaxBounds.call(this, bounds);
     if (!bounds) return result;
